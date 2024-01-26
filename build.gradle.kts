3e7600bd6fdadb3be2a4646124dc7530bb68a349
Mars <49356012+7003Mars@users.noreply.github.com>
2024-01-26 22:57:45 +0800

    commit one

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+    application
+}
+
+group = "me.mars"
+version = "1.0-SNAPSHOT"
+
+val lib by extra(Type.Skija)
+val lwjglVer by extra("3.3.1")
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.lwjgl:lwjgl:$lwjglVer")
+    implementation("org.lwjgl:lwjgl-glfw:$lwjglVer")
+    implementation("org.lwjgl:lwjgl-opengl:$lwjglVer")
+    runtimeOnly("org.lwjgl:lwjgl:$lwjglVer:natives-windows")
+    runtimeOnly("org.lwjgl:lwjgl-glfw:$lwjglVer:natives-windows")
+    runtimeOnly("org.lwjgl:lwjgl-opengl:$lwjglVer:natives-windows")
+
+    when(lib) {
+        Type.Skiko -> {
+            implementation("org.jetbrains.skiko:skiko:${lib.version}")
+            implementation("org.jetbrains.skiko:skiko-awt-runtime-windows-x64:${lib.version}")
+        }
+        Type.Skija -> {
+            implementation("io.github.humbleui:skija-shared:${lib.version}")
+            implementation("io.github.humbleui:skija-windows-x64:${lib.version}")
+        }
+    }
+}
+
+
+enum class Type(val version: String) {
+    Skiko("0.7.90"),
+    Skija("0.116.2")
+}
+
+application {
+    mainClass = "me.mars.MainKt"
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+package me.mars
+
+// Skija
+import io.github.humbleui.skija.DirectContext
+import io.github.humbleui.skija.impl.Library
+// Skiko
+//import org.jetbrains.skia.DirectContext
+//import org.jetbrains.skiko.Library
+
+
+import org.lwjgl.glfw.GLFW.*
+import org.lwjgl.system.MemoryUtil.NULL
+
+fun main() {
+    glfwInit()
+    val window: Long = glfwCreateWindow(100, 100, "Title", NULL, NULL)
+    if (window == NULL) throw RuntimeException("Failed to create window")
+    glfwMakeContextCurrent(window)
+
+
+//    Library.load()
+    println("Makegl?")
+    DirectContext.makeGL()
+    println("Made")
+}
